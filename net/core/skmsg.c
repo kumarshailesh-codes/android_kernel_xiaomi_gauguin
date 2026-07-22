3f087d8059925eccf7a7716a1c9c0ed651516f35
kumarshailesh-codes <kumarshailesh12qw4@gmail.com>
2026-07-22 13:38:35 +0000

    net: skmsg: Move sk_psock_from_strp to avoid forward declaration warning
    Since sk_psock_from_strp is invoked inside sk_psock_strp_read, but only
    defined later in the file, it can trigger compiler warnings or errors
    depending on strict build settings. Move the helper function definition
    to the top of the file before its first use to resolve the issue.
    
    Based-on-work-by: liuweien339 <liuweien339@gmail.com>
    Signed-off-by: Ryxpace <ryxpace@proton.me>

--- net/core/skmsg.c
+++ net/core/skmsg.c
@@ -786,6 +786,14 @@ static void sk_psock_write_space(struct sock *sk)
 }
 
 #if IS_ENABLED(CONFIG_BPF_STREAM_PARSER)
+static struct sk_psock *sk_psock_from_strp(struct strparser *strp)
+{
+	struct sk_psock_parser *parser;
+
+	parser = container_of(strp, struct sk_psock_parser, strp);
+	return container_of(parser, struct sk_psock, parser);
+}
+
 static void sk_psock_strp_read(struct strparser *strp, struct sk_buff *skb)
 {
 	struct sk_psock *psock = sk_psock_from_strp(strp);
@@ -807,14 +815,6 @@ static void sk_psock_strp_read(struct strparser *strp, struct sk_buff *skb)
 static int sk_psock_strp_read_done(struct strparser *strp, int err)
 {
 	return err;
-}
-
-static struct sk_psock *sk_psock_from_strp(struct strparser *strp)
-{
-	struct sk_psock_parser *parser;
-
-	parser = container_of(strp, struct sk_psock_parser, strp);
-	return container_of(parser, struct sk_psock, parser);
 }
 
 static int sk_psock_strp_parse(struct strparser *strp, struct sk_buff *skb)
